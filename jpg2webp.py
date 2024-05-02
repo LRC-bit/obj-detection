f6c31e542af736955c518964c9c99bd543730f70
LRC-bit <72396848+LRC-bit@users.noreply.github.com>
2024-05-02 19:41:34 +0800

    Add files via upload

---
+++ jpg2webp.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import os
+from multiprocessing import Pool
+
+def process_image_train(i, file_name):
+    rgb_path = f"/kaggle/working/GAIIC2024-赛道1-目标检测任务/{file_name}/rgb/{i}.jpg"
+    gray_path = f"/kaggle/working/GAIIC2024-赛道1-目标检测任务/{file_name}/tir/{i}.jpg"
+    output_path = f"kaggle/working/dataset/{file_name}/{i}.webp"
+    # 直接创建一个与RGB图像相同大小的RGBA图像，并将灰度图作为Alpha通道
+    img_combined = Image.merge("RGBA", (*Image.open(rgb_path).split(), Image.open(gray_path).convert("L")))
+
+    # 保存为带透明通道的WebP图片
+    img_combined.save(output_path, format="WEBP")
+
+
+if __name__ == "__main__":
+    for file_name in [("train",17991), ("val",1550), ("test",1001)]:
+        os.makedirs(f"kaggle/working/dataset/{file_name[0]}")
+        with Pool(processes=8) as pool:  # 假设使用8个进程
+            pool.map(process_image_train, [(i, file_name[0]) for i in range(1, file_name[1])])
